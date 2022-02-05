3cab5d659e8823daebf6de212e639f2ef81a47ef
namdoiz <nnamdi.on7@gmail.com>
2022-02-05 17:45:59 -0600

    Practice Problem 6

---
+++ pp6_lesson5.rb
@@ -0,0 +1,34 @@
+=begin
+
+One of the most frequently used real-world string
+properties is that of "string substitution", 
+where we take a hard-coded string and modify it 
+with various parameters from our program.
+
+Given this previously seen family hash, 
+print out the name, age and gender of each 
+family member:
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+...like this:
+(Name) is a (age)-year-old (male or female).
+=end
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |k,y|
+  p "#{k} is a #{y['age']}-year-old #{y['gender']}."
+end
