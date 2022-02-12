46da1d563d2e40ebc02ddd7e65989e445e8ad61c
namdoiz <nnamdi.on7@gmail.com>
2022-02-12 16:44:56 -0600

    Practice Problem 16

---
+++ pp16_lesson5.rb
@@ -0,0 +1,101 @@
+# Write a method that returns one UUID when called with no parameters.
+
+def letter_choice
+  x = rand(0...26)
+  alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+  choice = alphabet[x]
+  choice
+end 
+
+
+def num_choice
+  x = rand(0...9)
+  x
+end
+
+
+def loop_one
+  char1 = ''
+  loop do
+    x = rand(1..2)
+    if x == 1
+      loop do
+        char1 << letter_choice 
+        break
+      end
+    else
+      loop do
+        char1 << num_choice.to_s
+        break
+      end
+    end
+    break if char1.size == 8
+  end
+  char1 << '-'
+  loop do
+    x = rand(1..2)
+    if x == 1
+      loop do
+        char1 << letter_choice 
+        break
+      end
+    else
+      loop do
+        char1 << num_choice.to_s
+        break
+      end
+    end
+    break if char1.size == 13
+  end
+  char1 << '-'
+  loop do
+    x = rand(1..2)
+    if x == 1
+      loop do
+        char1 << letter_choice 
+        break
+      end
+    else
+      loop do
+        char1 << num_choice.to_s
+        break
+      end
+    end
+    break if char1.size == 18
+  end
+  char1 << '-'
+  loop do
+    x = rand(1..2)
+    if x == 1
+      loop do
+        char1 << letter_choice 
+        break
+      end
+    else
+      loop do
+        char1 << num_choice.to_s
+        break
+      end
+    end
+    break if char1.size == 23
+  end
+  char1 << '-'
+  loop do
+    x = rand(1..2)
+    if x == 1
+      loop do
+        char1 << letter_choice 
+        break
+      end
+    else
+      loop do
+        char1 << num_choice.to_s
+        break
+      end
+    end
+    break if char1.size == 32
+  end
+  char1
+end
+
+p loop_one
